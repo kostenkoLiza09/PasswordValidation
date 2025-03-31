0196f3129871f9c04d721a497ba3d4fe951dab1e
kostenkoLiza09 <kostaz063@gmail.com>
2025-03-31 12:11:28 +0200

    Added methods and tests for password

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -1,2 +1,49 @@
-package org.example;public class PasswordValidator {
+package org.example;
+
+public class PasswordValidator {
+
+    public boolean isLengthValid(String password) {
+        return password.length() >= 8;
+    }
+
+    public boolean hasDigit(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isDigit((i))) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public boolean checkUpperLower(String password) {
+        boolean lower = false;
+        boolean upper = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char pass = password.charAt(i);
+
+            if (Character.isLowerCase(pass)) {
+                lower = true;
+            }
+            if (Character.isUpperCase(pass)) {
+                upper = true;
+            }
+
+            if (lower && upper) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public boolean usedPassword(String newPassword, String[] passwords) {
+        for (String p : passwords) {
+            if (newPassword.equals(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -1,4 +1,40 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PasswordValidatorTest {
-  
+
+    @Test
+    void isLengthValid() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        assertTrue(passwordValidator.isLengthValid("12345678"));
+        assertFalse(passwordValidator.isLengthValid("123"));
+    }
+
+    @Test
+    void checkNumber() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        assertTrue(passwordValidator.hasDigit("Pass1"));
+        assertTrue(passwordValidator.hasDigit("Pass"));
+    }
+
+    @Test
+    void hasUpperLower() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        assertTrue(passwordValidator.checkUpperLower("PPPPPaaaaa"));
+    }
+
+    @Test
+    void usedPassword() {
+
+        String passwords[] = {"Password", "Password1"};
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        String isUsed = "Password";
+        String isNotUsed = "NewPassword";
+        assertTrue(passwordValidator.usedPassword(isUsed, passwords));
+        assertFalse(passwordValidator.usedPassword(isNotUsed, passwords));
+    }
 }
